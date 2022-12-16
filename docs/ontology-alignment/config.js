0d8b56fc5ce2b6c0c4365fcffc6884289fdb03f7
RiX012 <RiX012@users.noreply.github.com>
2022-12-16 13:20:20 +0000

    Initialize new ReSpec repository
    Reason: run setup.yaml after clone by RiX012

---
+++ docs/ontology-alignment/config.js
@@ -0,0 +1,15 @@
+var respecConfig = {
+  // => https://github.com/stichting-crow/respec/wiki
+  specStatus: "DRAFT",
+  shortName: "ontology-alignment/ontology-alignment",
+  specType: "technische documentatie", // vrije tekst, kleine letter
+  imprint: "crow",
+  editors: [
+    // alleen `name` is verplicht, https://respec.org/docs/#person
+    {
+      name: "Rik",
+    },
+  ],
+  github: "https://github.com/Stichting-CROW/ontology-alignment",
+  license: "undefined",
+};
